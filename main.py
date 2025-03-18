2f1551fceb271a296064d2ead3c9862c2b272b0e
higakaga <higakaga@gmail.com>
2025-03-18 19:03:53 +0900

    Request Body

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from enum import Enum
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 class ModelName(str. Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
 
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 app = FastAPI()
 
@@ -81,3 +87,18 @@ async def get_model(model_name: ModelName):
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
