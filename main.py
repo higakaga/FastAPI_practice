3d76762a12a91a26961f6355d446e485176f91dd
higakaga <higakaga@gmail.com>
2025-03-22 22:29:35 +0900

    Body - Multiple Parameters

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Union, List, Annotated, Literal
 
-from fastapi import FastAPI, Query, Path
+from fastapi import FastAPI, Query, Path, Body
 from pydantic import BaseModel, Field
 
 class ModelName(str, Enum):
@@ -11,9 +11,13 @@ class ModelName(str, Enum):
 
 class Item(BaseModel):
     name: str
-    description: str | None = None
+    description: Union[str, None] = None
     price: float
-    tax: float | None = None
+    tax: Union[float, None] = None
+
+class User(BaseModel):
+    username: str
+    full_name: Union[str, None] = None
 
 class FilterParams(BaseModel):
     model_config = {"extra": "forbid"}
@@ -171,8 +175,19 @@ async def create_item(item: Item):
     return item_dict
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item, q: str | None = None):
-    result = {"item_id": item_id, **item.dict()}
+async def update_item(
+    *,
+    item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
+    item: Item,
+    user: User,
+    importance: int = Body(gt=0),
+    q: Union[str, None] = None,
+    ):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
     if q:
-        result.update({"q": q})
-    return result
+        results.update({"q": q})
+    return results
+
+# async def update_item(item_id: int, item: Item = Body(embed=True)):
+#     results = {"item_id": item_id, "item": item}
+#     return results
